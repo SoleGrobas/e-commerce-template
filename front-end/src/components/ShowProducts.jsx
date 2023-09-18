d04c9e803053b2db32106b4c86aa2a3f545cb1c1
SoleGrobas <solegrobas@yahoo.com.ar>
2023-09-18 16:18:36 +0200

    Front-end conected to back-end via axios, ShowProducts example table is working

---
+++ front-end/src/components/ShowProducts.jsx
@@ -0,0 +1,66 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
+
+const endpoint = 'http://127.0.0.1:8000/api'
+
+const ShowProducts = () => {
+    const [ products, setProducts ] = useState([])
+    useEffect ( ()=> {
+        getAllProducts()
+    }, [])
+
+    const getAllProducts = async() =>{
+        const response = await axios.get(`${endpoint}/products`)
+        setProducts(response.data)
+        console.log(response.data)
+    }
+
+    const deleteProduct = async (id) => {
+        await axios.delete(`${endpoint}/product/${id}`)
+        getAllProducts()
+
+    }
+  return (
+    <div>
+      <div className='d-grid gap-2'>
+        <Link to="/create" className='btn btn-success btn-lg mt-2 mb-2 text-white'>Crear</Link>
+      </div>
+
+      <table className='table table-striped'>
+        <thead className='bg-pimary text-white'>
+            <tr>
+                <th>title</th>
+                <th>description</th>
+                <th>price</th>
+                <th>discount</th>
+                <th>stock</th>
+                <th>image</th>
+                <th>size</th>
+                <th>Acciones</th>
+            </tr>
+
+        </thead>
+        <tbody>
+           {products.map((product)=>(
+            <tr key={product.id}>
+                <td>{product.title}</td>
+                <td>{product.description}</td>
+                <td>{product.price}</td>
+                <td>{product.discount}</td>
+                <td>{product.stock}</td>
+                <td>{product.image}</td>
+                <td>{product.size}</td>
+                <td>
+                    <Link to={`/edit/${product.id}`} className='btn btn-warning'>Editar</Link>
+                    <button onClick={ ()=>deleteProduct(product.id)} className='btn btn-danger'>Eliminar</button>
+                </td>
+            </tr>
+           ))} 
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default ShowProducts;
